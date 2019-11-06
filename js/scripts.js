e0429c13e79cf65b58bc0b7b2bef52fa382d1358
Student <example@example.com>
2019-11-06 09:55:23 -0800

    set framework for css/html display, make constructors and prototypes for players and score modifiers

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,37 @@
+function Players (score1, score2){
+  this.score1 = 0,
+  this.score2 = 0
+}
+var dice = 0
+
+Players.prototype.addScore1 = function(dice) {
+  if (dice === 1) {
+    this.score1 = 0
+  } else {
+    this.score1 += dice
+  }
+}
+
+Players.prototype.addScore2 = function(dice) {
+  if (dice === 1) {
+    this.score2 = 0
+  } else {
+    this.score2 += dice
+  }
+}
+
+
+var roll = function(){
+  dice = Math.floor(Math.random() * 6) +1;
+
+}
+
+
 $(document).ready(function(){
+  var players = new Players(0, 0)
+  roll()
+  console.log(dice);
+  players.addScore1(dice);
+  console.log(players.score1);
 
 });
