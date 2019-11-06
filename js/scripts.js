3b59330fc052cf38642fbbe322b7cd0a1de35ffa
Student <example@example.com>
2019-11-06 15:50:37 -0800

    add ai branch

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ function Players (score1, score2){
   this.score1 = score1,
   this.score2 = score2
 }
+
+var isAiTurn = false
 var dice = 0
 var turnScore = 0
 
@@ -26,7 +28,49 @@ var roll = function(){
   dice = Math.floor(Math.random() * 6) +1;
 
 }
+
+
+
 var players = new Players(0,0)
+
+var aiPass = function() {
+  console.log(turnScore);
+  players.addScore2(turnScore);
+  if (players.score2 >= 100){
+    $(".game-area").hide();
+    $(".winner").show();
+    $("#winner-name").text($("#player2").val());
+  }
+  $("#p2turn").hide();
+  $("#p1turn").show();
+  $("#p1-roll-button").show();
+  $("#single-player-pass").show();
+  $("#p2score").text(players.score2);
+  dice = 0
+  turnScore = 0
+  $("#current-roll").text(dice);
+  $("#turn-score").text(dice);
+  isAiTurn = false
+}
+
+var singlePlayerPass = function(){
+  players.addScore1(turnScore);
+  if (players.score1 >= 100){
+    $(".game-area").hide();
+    $(".winner").show();
+    $("#winner-name").text($("#player1").val());
+  }
+  $("#p1turn").hide();
+  $("#p2turn").show();
+  $("#p1-roll-button").hide();
+  $("#single-player-pass").hide();
+  $("#p1score").text(players.score1);
+  dice = 0
+  turnScore = 0
+  $("#current-roll").text(dice);
+  $("#turn-score").text(dice);
+  isAiTurn = true
+}
 
 var p1Pass = function () {
   players.addScore1(turnScore);
@@ -69,6 +113,26 @@ var p2Pass = function (){
 }
 
 $(document).ready(function(){
+  $("#player1mode").click(function(){
+    var player1 = "Player 1"
+    var player2 = "Computer"
+    $("#p1name").text(player1);
+    $("#p2name").text(player2);
+    $(".game-select").hide();
+    $("#p1pass").hide();
+    $("#p1score").text(players.score1);
+    $("#p2score").text(players.score2);
+    $("#single-player-pass").show();
+    $("#p1-roll-button").show();
+    $("#p2pass").hide();
+    $("#p2-roll-button").hide();
+    $(".game-area").show()
+  })
+  $("#player2mode").click(function(){
+    $(".game-select").hide();
+    $("#single-player-pass").hide();
+    $("#players").show();
+  })
   $("#players").submit(function(event) {
     event.preventDefault ();
     var player1 = $("#player1").val();
@@ -111,6 +175,10 @@ $(document).ready(function(){
     currentScore();
     console.log(turnScore);
     $("#turn-score").text(turnScore);
+  })
+  $("#single-player-pass").click(function(){
+    singlePlayerPass();
+    console.log(isAiTurn);
   })
   $("#p1pass").click(function(){
     p1Pass();
